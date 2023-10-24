9fb381719eee185755d3c63d3901749475ecb1b4
manuelfc08 <mfernandezc22@fpcoruna.afundacion.org>
2023-10-24 14:27:33 +0200

    Ejer 3,4,5 Unos funcionan otros no

--- sprint2sql/www/mysite/detail.php
+++ sprint2sql/www/mysite/detail.php
@@ -1,38 +1,63 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+$db = mysqli_connect("localhost", "root", "1234", "mysitedb") or die("Fail");
 ?>
+<!DOCTYPE html>
 <html>
+<head>
+    <meta charset="UTF-8">
+    <style>
+        * {
+            text-align: center;
+        }
+
+        h1 {
+            text-align: center;
+            color: black;
+            background-color: bisque;
+
+        }
+
+        div {
+            float: left;
+        }
+
+        div * {
+            text-align: left;
+        }
+    </style>
+</head>
 
 <body>
     <?php
     if (!isset($_GET['libro_id'])) {
-        die('No se ha especificado un libro');
+        die("El id indicado no corresponde a ningún libro");
     }
-    echo 'prueba';
     $libro_id = $_GET['libro_id'];
-    $query = 'SELECT * FROM tLibros WHERE id=' . $libro_id;
-    $result = mysqli_query($db, $query) or die('Query error');
+    $query = "SELECT * FROM tLibros where id=".$libro_id;
+    $result = mysqli_query($db, $query) or die("Query error");
     $only_row = mysqli_fetch_array($result);
-    echo '<h1>' . $only_row['nombre'] . '</h1>';
-    echo '<h2>' . $only_row['isbn'] . '</h2>';
+    echo "<h1>" . $only_row["nombre"] . "</h1>";
+    echo "<img src='" . $only_row["url_imagen"] . "' alt='" . $only_row["nombre"] . "' height='340px' width='220px' border='2px' hspace='30px'/>";
+    echo "<p>Género: " . $only_row["genero"] . "&nbsp;&nbsp;&nbsp;&nbsp;Género: " . $only_row["genero"] . "</p>";
     ?>
-    <h3>Comentarios:</h3>
-    <ul>
+    <div>
+        <h2 style="text-align: left;">Comentarios:</h2>
         <?php
-        $query2 = 'SELECT * FROM tComentarios WHERE libro_id=' . $libro_id;
-        $result2 = mysqli_query($db, $query2) or die('Query error');
+        $query2 = "SELECT * FROM tComentarios where id=" . $libro_id;
+        $result2 = mysqli_query($db, $query2) or die("Query error");
         while ($row = mysqli_fetch_array($result2)) {
-            echo '<li>' . $row['comentario'] . '</li>';
+            echo "<p>Usuario: " . $row["usuario_id"] . " || " . $row["comentario"] . "</p>";
         }
         mysqli_close($db);
         ?>
-    </ul>
-    <p>Deja un nuevo comentario:</p>
-    <form action="/comment.php" method="post">
-        <textarea rows="4" cols="50" name="new_comment"></textarea><br>
-        <input type="hidden" name="libro_id" value="<?php echo $libro_id; ?>">
-        <input type="submit" value="Comentar">
-    </form>
+        <br>
+        <p>Deja un nuevo comentario:</p>
+        <form action="./comment.php" method="post">
+            <textarea rows="4" cols="50" name="new_comment"></textarea>
+            <input type="hidden" name="libro_id" value="<?php echo $libro_id; ?>">
+            <input type="submit" value="Comentar">
+        </form>
+    </div>
 </body>
 
 </html>
--- sprint2sql/www/mysite/main.php
+++ sprint2sql/www/mysite/main.php
@@ -1,13 +1,58 @@
+<?php
+$db = mysqli_connect("localhost", "root", "1234", "mysitedb") or die("Fail");
+?>
+<!DOCTYPE html>
 <html>
-	<head>
-		<meta charset = "UTF-8">
 
-	</head>
-	<body>
-		
-		<?php
-			$db = mysqli_connect('localhost','root','1234','mysitedb') or die('Fail');
-		?>
-		<h1> Conexión establecida</h1>
-	</body>
+<head>
+	<title>LIBRERÍA</title>
+	<meta charset="UTF-8">
+	<style>
+		table {
+			border: 2px solid black;
+			background-color: #D4C8AA;
+			margin: auto;
+		}
+
+		th {
+			border: inherit;
+			background-color: rgb(229, 178, 108);
+
+		}
+
+		td {
+			border: 1px solid black;
+			text-align: center;
+			padding: 15px;
+		}
+	</style>
+</head>
+
+<body>
+	<?php
+	if (!isset($_GET['libro_id'])) {
+		echo "<table>";
+		echo "<tr>";
+		echo "<th>ID</th>";
+		echo "<th>Imagen</th>";
+		echo "<th>Nombre</th>";
+		echo "<th>Género</th>";
+		echo "<th>ISBN</th>";
+		echo "</tr>";
+		$query = "SELECT * FROM tLibros";
+		$result = mysqli_query($db, $query) or die("Query error");
+		while ($row = mysqli_fetch_array($result)) {
+			echo "<tr>";
+			echo "<td><a href='/detail.php?libro_id=" . $row["id"] . "' target='_self'>" . $row["id"] . "</a></td>";
+			echo "<td><img src='" . $row["url_imagen"] . "' alt='" . $row["nombre"] . "' height='340px' width='220px' border='2px' hspace='30px'></td>";
+			echo "<td>" . $row["nombre"] . "</td>";
+			echo "<td>" . $row["genero"] . "</td>";
+			echo "<td>" . $row["isbn"] . "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	}
+	?>
+</body>
+
 </html>
